5f13a32525d227f1b50c088ad19b3c19d4a6a9e0
shanujoseph32 <shanujoseph32@gmail.com>
2025-03-31 04:44:46 +0530

    eeeee

--- api/submit.ts
+++ api/submit.ts
@@ -1,10 +1,10 @@
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
-    res.status(200).end(); // Handle preflight request
+    res.status(200).end();
     return;
   }
 
